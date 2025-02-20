7ff236ee8c5981b40855f5bc74d2b995ba402b32
babi <barbaracarolynep@gmail.com>
2025-02-20 14:40:34 -0300

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,10 @@
+print("Informe seu salário:")
+salario = float(input())
+
+print("Informe seu saldo:")
+saldo = float(input())
+
+Empréstimo = salario * 3 + saldo / 5
+
+print(f"Seu empréstimo maximo é: {Empréstimo:.2f}")
+#para que o valor saia com duas casas decimais >.<
